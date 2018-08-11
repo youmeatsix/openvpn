e35b5b5a9a64c47f1fbbd95976b4c1deb2d46606
Lars Klitzke <Lars@Klitzke-web.de>
2018-08-11 16:19:01 +0200

    wait until the user uploaded the configuration files

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-CLIENT_CONFIG_LOCATION=/share/openvpnclient/client.ovpn
+declare -r REQUIRED_FILES="client.crt client.key ca.crt"
+declare -r CLIENT_CONFIG_LOCATION=/share/openvpnclient
 
 # create the tunnel for the openvpn client
 mkdir -p /dev/net
@@ -10,9 +11,31 @@ fi
 
 cd /
 
-jq --raw-output '.ovpn-config' /options.json > ${CLIENT_CONFIG_LOCATION}
+jq --raw-output '.ovpn-config' /options.json > ${CLIENT_CONFIG_LOCATION}/client.ovpn
 
-chmod 777 ${CLIENT_CONFIG_LOCATION}
+chmod 777 ${CLIENT_CONFIG_LOCATION}/client.ovpn
+
+
+# therefore, wait until the user upload the required certification files
+while true; do
+
+    failed=0
+    for file in ${REQUIRED_FILES}
+    do
+        if [[ ! -f ${CLIENT_CONFIG_LOCATION}/${file} ]]
+        then
+            failed=1
+            break
+        fi
+    done
+
+    if [[ ${failed} == 0 ]]
+    then
+        break
+    fi
+
+    sleep 5
+done
 
 # star the client
-openvpn --config ${CLIENT_CONFIG_LOCATION}
+openvpn --config ${CLIENT_CONFIG_LOCATION}/client.ovpn
