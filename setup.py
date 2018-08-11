c9197012cc8798548ad26edf5cee33a00af56755
Lars Klitzke <Lars@Klitzke-web.de>
2018-08-11 15:38:22 +0200

    init setup script to install the Flask applicaiton

---
+++ setup.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2016-2018 by Lars Klitzke, lars@klitzke-web.de
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+import setuptools
+import os
+
+# read in the long description written in the README.md file
+with open(os.path.join(os.path.dirname(__file__), "README.md"), "r") as fh:
+    long_description = fh.read()
+
+__version__ = '0.1.2'
+
+setuptools.setup(
+    name='openvpnclient',
+    version=__version__,
+    description='',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Lars Klitzke',
+    author_email='Lars@klitzke-web.de',
+    entry_points={
+        'console_scripts': [
+            'openvpnclient = openvpnclient.app:main',
+        ]
+    },
+    packages=setuptools.find_packages('.'),
+    install_requires=[
+        'flask',
+    ],
+)
