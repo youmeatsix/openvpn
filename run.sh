52a72ad81910f17868c5ccb38df3bf03831fbea0
Lars Klitzke <Lars@Klitzke-web.de>
2018-08-11 15:38:07 +0200

    let the user define the open vpn configuration using the homeassistant addon-on configuration panel.

--- run.sh
+++ run.sh
@@ -1,8 +1,18 @@
+#!/bin/bash
+
+CLIENT_CONFIG_LOCATION=/share/openvpnclient/client.ovpn
+
+# create the tunnel for the openvpn client
 mkdir -p /dev/net
 if [ ! -c /dev/net/tun ]; then
     mknod /dev/net/tun c 10 200
 fi
 
-jq --raw-output '.config' /data/options.json > /openvpn.conf
-jq --raw-output '.key' /data/options.json > /openvpn.key
-openvpn --secret /openvpn.key --config /openvpn.conf
+cd /
+
+jq --raw-output '.ovpn-config' /options.json > ${CLIENT_CONFIG_LOCATION}
+
+chmod 777 ${CLIENT_CONFIG_LOCATION}
+
+# star the client
+openvpn --config ${CLIENT_CONFIG_LOCATION}
